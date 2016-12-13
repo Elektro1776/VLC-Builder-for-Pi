7bbd371315a1199dfbe4704975c4dc67e49976b4
Karanvir Singh <karanvir.singh61@yahoo.in>
2016-12-13 17:19:14 +0530

    Update vlc_hw_acceleration_on_pi_compiler_and_builder.sh

--- vlc_hw_acceleration_on_pi_compiler_and_builder.sh
+++ vlc_hw_acceleration_on_pi_compiler_and_builder.sh
@@ -24,17 +24,15 @@ echo "Installing hardware acceleration modules..."
 sudo apt-get install liba52-0.7.4-dev libdirac-dev libdvdread-dev libkate-dev libass-dev libbluray-dev libcddb2-dev libdca-dev libfaad-dev libflac-dev libmad0-dev libmodplug-dev libmpcdec-dev libmpeg2-4-dev libogg-dev libopencv-dev libpostproc-dev libshout3-dev libspeex-dev libspeexdsp-dev libssh2-1-dev liblua5.1-0-dev libopus-dev libschroedinger-dev libsmbclient-dev libtwolame-dev libx264-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-xv0-dev libzvbi-dev
 echo "Installing qt libs..."
 sudo apt-get install qtcreator
-echo "Now compiling, this will take about six hours..."
-./configure --enable-rpi-omxil --enable-dvbpsi --enable-x264
-make clean all
+echo "Now compiling, this may take few hours..."
+sudo ./configure --enable-rpi-omxil --enable-dvbpsi --enable-x264
+sudo make clean
+sudo make
 echo "Now installing..."
 sudo make install
-sudo ln -s /usr/local/lib/libvlc* /usr/lib/
-sudo ln -s /usr/local/lib/libx264.a /usr/lib/
-sudo ln -s /usr/local/lib/vlc /usr/lib/vlc
 echo "Installation complete"
 echo "Trying to run vlc..."
-vlc --vout omxil_vout
+sudo ./vlc --vout omxil_vout
 read -p "Cleanup and delete source to save disk space? [y/n]";
 if [ $REPLY == "y" ]; then
 	cd ..
